4dc27803116b08abe51714a483d805713d557665
Leksa <lexysnake@gmail.com>
2023-04-04 18:54:05 +0300

    lesson 5

---
+++ js/global.js
@@ -0,0 +1,8 @@
+const box = document.querySelector(".container");
+const addBtn = document.querySelector(".add-btn");
+const mdBox = document.querySelector(".modal-container");
+const mdClose = mdBox.querySelector(".modal-close");
+const addForm = document.forms.add;
+
+const user = "lk12";
+const path = `https://cats.petiteweb.dev/api/single/${user}`;
